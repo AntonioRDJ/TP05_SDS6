ced26d03df0732fbd285eb6aa49f350655b8bdc3
Antonio Rodrigues <antonio.rodrdj@gmail.com>
2022-09-21 23:58:45 -0300

    first commit

---
+++ TP05/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace TP05
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputPath = new System.Windows.Forms.TextBox();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(113, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Verificar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Path do xml";
+            // 
+            // inputPath
+            // 
+            this.inputPath.Location = new System.Drawing.Point(129, 31);
+            this.inputPath.Name = "inputPath";
+            this.inputPath.Size = new System.Drawing.Size(213, 23);
+            this.inputPath.TabIndex = 2;
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(27, 124);
+            this.textBoxMessage.Multiline = true;
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.ReadOnly = true;
+            this.textBoxMessage.Size = new System.Drawing.Size(346, 90);
+            this.textBoxMessage.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 226);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.inputPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputPath;
+        private System.Windows.Forms.TextBox textBoxMessage;
+    }
+}
+
---
+++ TP05/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace TP05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = inputPath.Text;  //./voosCorreto.xml ./voosErro.xml
+            try
+            {                            // Set the validation settings.
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.DtdProcessing = DtdProcessing.Parse;
+                settings.ValidationType = ValidationType.DTD;
+                settings.ValidationEventHandler += new ValidationEventHandler(ValidationCallBack);
+                // Referências externas no XML
+                settings.XmlResolver = new XmlUrlResolver();
+                // Create the XmlReader object.
+                XmlReader reader = XmlReader.Create(path, settings);
+                // Parse the file. 
+                while (reader.Read()) ;
+                textBoxMessage.Text = "Analisado!";
+            }
+            catch (Exception exception)
+            {
+                textBoxMessage.Text = exception.Message;
+            }
+        }
+
+        private void ValidationCallBack(object sender, ValidationEventArgs e)
+        {
+            textBoxMessage.Text = "Validation Error: " + e.Message;
+        }
+
+    }
+}
